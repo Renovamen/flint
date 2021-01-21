fix: handle broadcast when backwarding

test/linear.py
@@ -21,14 +21,14 @@ class MLP(nn.Module):
         out = self.l1(x)
         return out
 
-n_epoch = 3
+n_epoch = 5
 lr = 0.5
 
 np.random.seed(0)
 
 in_features = 5
 out_features = 2
-batch_size = 1
+batch_size = 3
 
 inputs = np.random.rand(batch_size, in_features)
 labels = np.random.randint(0, out_features, (batch_size, ))
@@ -48,7 +48,7 @@ for i in range(n_epoch):
     optimer.zero_grad()
 
     pred = net(x)
-    print('Prediction: ', pred.data)
+    # print('Prediction: ', pred.data)
 
     loss = loss_function(pred, y)
     print('Loss: ', loss.data)

tinyark/functional.py
@@ -47,11 +47,12 @@ def nll_loss(
 
     def grad_nll():
         if input.requires_grad:
+            p = np.clip(input.data, 1e-15, 1 - 1e-15)
             y = to_categorical(target.data)
             if reduction == 'mean':
-                input.grad += (input.data - y) / batch_size  # (batch_size, n_classes)
+                input.grad += (p - y) / batch_size  # (batch_size, n_classes)
             elif reduction == 'sum':
-                input.grad += (input.data - y)  # (batch_size, n_classes)
+                input.grad += (p - y)  # (batch_size, n_classes)
 
     if out.requires_grad and reduction != 'none':
         out.grad_fn = grad_nll