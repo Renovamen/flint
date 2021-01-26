fix: correct autograding on softmax

test/test_tensor.py
@@ -141,7 +141,6 @@ class TestTensor(unittest.TestCase):
             return b.detach().numpy(), a.grad.numpy()
         
         for x, y in zip(test_tinyark(), test_torch()):
-            print(x, y)
             np.testing.assert_allclose(x, y, atol=1e-5)
 
     def test_sum(self):
@@ -156,6 +155,24 @@ class TestTensor(unittest.TestCase):
             a = torch.tensor([1., 2., 3., 4.], requires_grad=True)
             b = a ** 2
             c = b.sum()
+            c.backward()
+            return c.detach().numpy(), a.grad.numpy()
+        
+        for x, y in zip(test_tinyark(), test_torch()):
+            np.testing.assert_allclose(x, y, atol=1e-5)
+    
+    def test_max(self):
+        def test_tinyark():
+            a = tinyark.Tensor([1., 2., 4., 4.], requires_grad=True)
+            b = a ** 2
+            c = b.max()
+            c.backward()
+            return c.data, a.grad
+
+        def test_torch():
+            a = torch.tensor([1., 2., 4., 4.], requires_grad=True)
+            b = a ** 2
+            c = b.max()
             c.backward()
             return c.detach().numpy(), a.grad.numpy()
         
@@ -184,7 +201,47 @@ class TestTensor(unittest.TestCase):
         
         for x, y in zip(test_tinyark(), test_torch()):
             np.testing.assert_allclose(x, y, atol=1e-5)
-      
+    
+    def test_softmax(self):
+        a_init = np.random.randn(5).astype(np.float32)
+
+        def test_tinyark():
+            a = tinyark.Tensor(a_init, requires_grad=True)
+            b = a.softmax()
+            c = b.sum()
+            c.backward()
+            return c.data, b.data, a.grad
+
+        def test_torch():
+            a = torch.tensor(a_init, requires_grad=True)
+            b = torch.nn.functional.softmax(a, dim=0)
+            c = b.sum()
+            c.backward()
+            return c.detach().numpy(), b.detach().numpy(), a.grad.numpy()
+        
+        for x, y in zip(test_tinyark(), test_torch()):
+            np.testing.assert_allclose(x, y, atol=1e-5)
+        
+    def test_log_softmax(self):
+        a_init = np.random.randn(5).astype(np.float32)
+
+        def test_tinyark():
+            a = tinyark.Tensor(a_init, requires_grad=True)
+            b = a.log_softmax()
+            c = b.sum()
+            c.backward()
+            return c.data, b.data, a.grad
+
+        def test_torch():
+            a = torch.tensor(a_init, requires_grad=True)
+            b = torch.nn.functional.log_softmax(a, dim=0)
+            c = b.sum()
+            c.backward()
+            return c.detach().numpy(), b.detach().numpy(), a.grad.numpy()
+        
+        for x, y in zip(test_tinyark(), test_torch()):
+            np.testing.assert_allclose(x, y, atol=1e-5)
+            
         
 if __name__ == '__main__':
     unittest.main()

tinyark/tensor.py
@@ -347,43 +347,57 @@ class Tensor(object):
 
         return out
     
-    def sum(self, axis: int = None) -> 'Tensor':
+    def sum(self, axis: int = None, keepdims: bool = False) -> 'Tensor':
         out = Tensor(
-            data = np.array([np.sum(self.data)]) if axis is None else np.sum(self.data, axis=axis),
+            data = np.sum(self.data, axis=axis, keepdims=keepdims),
             depends_on = [self],
             requires_grad = self.requires_grad
         )
 
         def grad_sum():
             if self.requires_grad:
-                sum_axis = [axis] if type(axis) is int else axis
-                expanded_shape = [1 if sum_axis is None or i in sum_axis else self.shape[i] for i in range(len(self.shape))]
-                expanded_grad = out.grad.reshape(expanded_shape) + np.zeros_like(self.data)
-                self.grad += expanded_grad
+                out_grad = out.grad
+                if out.ndim < self.ndim:
+                    sum_axis = [axis] if type(axis) is int else axis
+                    expanded_shape = [1 if sum_axis is None or i in sum_axis else self.shape[i] for i in range(len(self.shape))]
+                    out_grad = out_grad.reshape(expanded_shape)
+                self.grad += out_grad + np.zeros_like(self.data)
 
         if out.requires_grad:
             out.grad_fn = grad_sum
 
         return out
     
-    def softmax(self, axis: int = -1) -> 'Tensor':
-        ret = self.data - np.max(self.data, axis=axis, keepdims=True)
-        ret = np.exp(ret)
-        ret = ret / np.sum(ret.data, axis=axis, keepdims=True)
-        
+    def max(self, axis: int = None, keepdims: bool = False) -> 'Tensor':
         out = Tensor(
-            data = ret,
+            data = np.max(self.data, axis=axis, keepdims=keepdims),
             depends_on = [self],
             requires_grad = self.requires_grad
         )
 
-        def grad_softmax():
+        def grad_max():
             if self.requires_grad:
-                self.grad += out.grad * out.data * (1 - out.data)
+                out_grad = out.grad
+                out_data = out.data
+                if out.ndim < self.ndim:
+                    max_axis = [axis] if type(axis) is int else axis
+                    # here I don't use np.expand_dims(), because I have to deal
+                    # with the situation when axis = None
+                    expanded_shape = [1 if max_axis is None or i in max_axis else self.shape[i] for i in range(len(self.shape))]
+                    out_grad = out_grad.reshape(expanded_shape)
+                    out_data = out_data.reshape(expanded_shape)
+                mask = (self.data == out_data)
+                self.grad += mask * out_grad
 
         if out.requires_grad:
-            out.grad_fn = grad_softmax
+            out.grad_fn = grad_max
 
+        return out
+    
+    def softmax(self, axis: int = -1) -> 'Tensor':
+        out = self - self.max(axis=axis, keepdims=True)
+        out = out.exp()
+        out = out / out.sum(axis=axis, keepdims=True)
         return out
     
     def log_softmax(self, axis: int = -1) -> 'Tensor':